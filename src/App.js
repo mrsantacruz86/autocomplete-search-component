9c5f8c4df168faf82bccadb85f002345a71b7720
Nelson Diaz <abnerdc86@gmail.com>
2018-09-07 18:06:03 -0400

    playing with css properties

--- src/App.js
+++ src/App.js
@@ -25,16 +25,17 @@ class App extends Component {
     this.searchMatches(this.props.list, e.target.value);
   }
 
-  handleDropdown = (e) => {
-    document.getElementById("searchbox").dropdown();
-  }
-
   searchMatches = (arr, search) => {
-    this.setState({
-      filteredCountries: arr.filter((item) => (
-        item.substr(0, search.length).toUpperCase() === search.toUpperCase()
-      ))
-    });
+    if (search != "") {
+      this.setState({
+        filteredCountries: arr.filter((item) => (
+          item.substr(0, search.length).toUpperCase() === search.toUpperCase()
+        ))
+      });
+    } else {
+      this.setState({ filteredCountries: [] });
+    }
+
   }
 
   handleSelectFromList = (e) => {
@@ -45,38 +46,38 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sample autocomplete search</h1>
         </header>
         <br />
         <div className="container">
           <div>
-            <form>
+            <form autoComplete="off">
               <div className="form-group">
                 <label htmlFor="exampleInputEmail1">Country</label>
-                <div className="dropdown">
+                <div className="autocomplete">
                   <input type="text"
                     className="form-control"
                     id="searchbox"
                     placeholder="Start Typing"
                     onChange={this.handleChange}
-                    // onFocus={this.handleDropdown}
                     value={this.state.search}
-                    data-toggle="dropdown"
                   />
 
-                  <div class="dropdown-menu" aria-labelledby="searchbox">
-                    {this.state.filteredCountries.map(item => (
-                      <a class="dropdown-item" onClick={this.handleSelectFromList}>
+                  <ul className="list-group search-results" aria-labelledby="searchbox">
+                    {this.state.filteredCountries.map((item, index) => (
+                      <li class="list-group-item list-group-item-action align-items-start" key={`c-${index}`} onClick={this.handleSelectFromList}>
                         {item}
-                      </a>
+                      </li>
                     ))}
-                  </div>
+                  </ul>
 
                 </div>
 
               </div>
             </form>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui quas, consequatur doloribus voluptates magni suscipit amet! Nam at, aut quam eveniet voluptatem expedita explicabo nobis ratione deserunt. Voluptas eius quas ut sit autem dolores at voluptates, nobis laboriosam! Possimus minima praesentium minus deleniti non, odio ad officia corporis, impedit facilis temporibus sapiente. Deserunt, odio? Est praesentium aliquid assumenda, ex accusantium neque, quia placeat, dignissimos temporibus consequatur doloremque labore quibusdam obcaecati consequuntur eveniet quam id necessitatibus fugiat fugit tempora rem? Eaque atque maxime quos porro quam molestias iusto id similique deleniti exercitationem laboriosam hic rem, eveniet voluptatum eius necessitatibus perspiciatis?
+            </p>
           </div>
         </div>
       </div>
