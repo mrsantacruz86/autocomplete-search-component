3a35d6f076991c4e7fa229d50f6376f1c56be383
Nelson Diaz <abnerdc86@gmail.com>
2018-09-10 17:57:49 -0400

    Working oon the onClick event.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import textfilter from './utils/textFilter';
+import ListItem from './Components/ListItem';
 
 
 class App extends Component {
@@ -15,18 +14,17 @@ class App extends Component {
       toggle: false,
     }
     this.handleChange = this.handleChange.bind(this);
-    this.handleSelectFromList = this.handleSelectFromList.bind(this);
+    this.handleListClick = this.handleListClick.bind(this);
+    this.handleOnLoseFocus = this.handleOnLoseFocus.bind(this);
   }
 
   handleChange = (e) => {
     this.setState({ search: e.target.value });
-    console.log(e.target.value);
-    console.log(this.state.search);
     this.searchMatches(this.props.list, e.target.value);
   }
 
   searchMatches = (arr, search) => {
-    if (search != "") {
+    if (search !== "") {
       this.setState({
         filteredCountries: arr.filter((item) => (
           item.substr(0, search.length).toUpperCase() === search.toUpperCase()
@@ -38,8 +36,13 @@ class App extends Component {
 
   }
 
-  handleSelectFromList = (e) => {
-    this.setState({ search: e.target.text, filteredCountries: [] });
+  handleListClick = (text) => {
+    // console.log(e.target.text);
+    this.setState({ search: text, filteredCountries: [] });
+  }
+
+  handleOnLoseFocus = (text) => {
+    this.setState({ search: text, filteredCountries: [] });
   }
 
   render() {
@@ -60,29 +63,33 @@ class App extends Component {
                     id="searchbox"
                     placeholder="Start Typing"
                     onChange={this.handleChange}
+                    onBlur={this.handleOnLoseFocus}
                     value={this.state.search}
-                  />
-
-                  <ul className="list-group search-results" aria-labelledby="searchbox">
-                    {this.state.filteredCountries.map((item, index) => (
-                      <li class="list-group-item list-group-item-action align-items-start" key={`c-${index}`} onClick={this.handleSelectFromList}>
-                        {item}
-                      </li>
+                    />
+  
+                  <div className="list-group search-results" aria-labelledby="searchbox">
+                      {this.state.filteredCountries.map((item, index) => (
+                        <ListItem
+                          key={`${index}`}
+                          onClick={this.handleListClick}
+                          text={item}
+                          ind={this.state.search.length}
+                        />
                     ))}
-                  </ul>
+                  </div>
 
                 </div>
 
-              </div>
+                </div>
             </form>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui quas, consequatur doloribus voluptates magni suscipit amet! Nam at, aut quam eveniet voluptatem expedita explicabo nobis ratione deserunt. Voluptas eius quas ut sit autem dolores at voluptates, nobis laboriosam! Possimus minima praesentium minus deleniti non, odio ad officia corporis, impedit facilis temporibus sapiente. Deserunt, odio? Est praesentium aliquid assumenda, ex accusantium neque, quia placeat, dignissimos temporibus consequatur doloremque labore quibusdam obcaecati consequuntur eveniet quam id necessitatibus fugiat fugit tempora rem? Eaque atque maxime quos porro quam molestias iusto id similique deleniti exercitationem laboriosam hic rem, eveniet voluptatum eius necessitatibus perspiciatis?
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui quas, consequatur doloribus voluptates magni suscipit amet! Nam at, aut quam eveniet voluptatem expedita explicabo nobis ratione deserunt. Voluptas eius quas ut sit autem dolores at voluptates, nobis laboriosam! Possimus minima praesentium minus deleniti non, odio ad officia corporis, impedit facilis temporibus sapiente. Deserunt, odio? Est praesentium aliquid assumenda, ex accusantium neque, quia placeat, dignissimos temporibus consequatur doloremque labore quibusdam obcaecati consequuntur eveniet quam id necessitatibus fugiat fugit tempora rem? Eaque atque maxime quos porro quam molestias iusto id similique deleniti exercitationem laboriosam hic rem, eveniet voluptatum eius necessitatibus perspiciatis?
             </p>
           </div>
+          </div>
         </div>
-      </div>
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    export default App;
