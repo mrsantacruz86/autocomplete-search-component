987954beb183e74fd64fbcbcd00c9982a8fb9efc
Nelson Diaz <abnerdc86@gmail.com>
2018-10-15 02:11:33 -0400

    Get google map working
    update when the country selection changes is still missing.

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-// import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import GoogleMapReact from 'google-map-react';
 
 class CountryMap extends Component {
 
   render() {
-    const position = [this.props.lat, this.props.lng];
+    let center = {
+      lat: this.props.lat,
+      lng: this.props.lng
+    }
     return (
-      <div id="map-frame">
-
+      // Important! Always set the container height explicitly
+      <div style={{ height: '400px' , width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyB4wbNsCXGWAnl77rR9Rj_7X5Lt7AuGJEI" }}
+          defaultCenter={center}
+          defaultZoom={3}
+        />
       </div>
     );
   }
