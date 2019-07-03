a4b6418f471ab28b53edb0668955891db595ed8e
Nelson Diaz <abnerdc86@gmail.com>
2019-07-03 13:15:43 -0400

    placing the button by the side of searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import ReactDOM  from 'react-dom';
-import './App.css';
-import icon from './globe-search.svg';
-import API from './utils/API';
-import CountryCard from './Components/CountryCard';
+import "./App.css";
+import icon from "./globe-search.svg";
+import API from "./utils/API";
+import CountryCard from "./Components/CountryCard";
 
 class App extends Component {
   constructor(props) {
@@ -12,30 +12,31 @@ class App extends Component {
       search: "",
       selectedCountry: {},
       filteredCountries: []
-    }
+    };
   }
   handleChange = e => {
     this.setState({ search: e.target.value });
     this.searchMatches(this.props.list, e.target.value);
-  }
+  };
 
   searchMatches = (arr, search) => {
     if (search !== "") {
       this.setState({
-        filteredCountries: arr.filter((item) => (
-          item.substr(0, search.length).toUpperCase() === search.toUpperCase()
-        ))
+        filteredCountries: arr.filter(
+          item =>
+            item.substr(0, search.length).toUpperCase() === search.toUpperCase()
+        )
       });
     } else {
       this.setState({ filteredCountries: [] });
     }
-  }
+  };
   handleListClick = item => {
     console.log("Selected:", item);
     return this.setState({ search: item, filteredCountries: [] });
-  }
+  };
 
-  handleFormSubmit = (e) => {
+  handleFormSubmit = e => {
     e.preventDefault();
     console.log("Form Submitted");
     API.getCountry(this.state.search, country => {
@@ -45,14 +46,16 @@ class App extends Component {
         filteredCountries: []
       });
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">
-            SEARCH THE GL<img src={icon} alt="logo" className="titleIcon" />BE
+            SEARCH THE GL
+            <img src={icon} alt="logo" className="titleIcon" />
+            BE
           </h1>
         </header>
         <br />
@@ -61,37 +64,55 @@ class App extends Component {
             <form autoComplete="off" onSubmit={this.handleFormSubmit}>
               <div className="form-group">
                 <div className="autocomplete">
-                  <input type="text"
-                    className="form-control"
-                    id="searchbox"
-                    placeholder="Start Typing"
-                    onChange={this.handleChange}
-                    value={this.state.search}
-                  />
-                  <div className="list-group search-results" aria-labelledby="searchbox">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="searchbox"
+                      placeholder="Type a country name here..."
+                      onChange={this.handleChange}
+                      value={this.state.search}
+                    />
+                    <div className="input-group-append">
+                      <button
+                        id="search-btn"
+                        type="submit"
+                        className="btn btn-primary"
+                      >
+                        Search
+                      </button>
+                    </div>
+                  </div>
+                  <div
+                    className="list-group search-results"
+                    aria-labelledby="searchbox"
+                  >
                     {this.state.filteredCountries.map((item, index) => (
-                      <div className="list-group-item align-items-start"
+                      <div
+                        className="list-group-item align-items-start"
                         key={index}
                         onClick={() => this.handleListClick(item)}
                       >
-                        <strong>{item.substr(0, this.state.search.length)}</strong>
+                        <strong>
+                          {item.substr(0, this.state.search.length)}
+                        </strong>
                         {item.substr(this.state.search.length)}
                       </div>
                     ))}
                   </div>
                 </div>
-
               </div>
-              <button id="search-btn" type="submit" className="btn btn-primary">Search</button>
             </form>
             <br />
 
             <div className="country-info">
-              {
-                this.state.selectedCountry.name ?
-                <CountryCard {...this.state.selectedCountry} /> :
-                  <p className="text-center">Please select a country to see its data.</p>
-              }
+              {this.state.selectedCountry.name ? (
+                <CountryCard {...this.state.selectedCountry} />
+              ) : (
+                <p className="text-center">
+                  Please select a country to see its data.
+                </p>
+              )}
             </div>
           </div>
         </div>
