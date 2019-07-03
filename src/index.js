510a350821e41198ae3bc697bedf9cbfb038340a
Nelson Diaz <abnerdc86@gmail.com>
2019-07-03 14:14:44 -0400

    deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import API from './utils/API';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import API from "./utils/API";
 
-
-API.getCountryNames( countries => {
-	ReactDOM.render(<App list={countries}/>, document.getElementById('root'));
+API.getCountryNames(countries => {
+  ReactDOM.render(<App list={countries} />, document.getElementById("root"));
 });
-
-registerServiceWorker();
